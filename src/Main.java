58d1c1e83ec13f8bd85a5e3751d14d70bad9404c
Skrety <kuba2286@gmail.com>
2020-06-10 17:19:30 +0200

    first commit at new branch

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,8 @@ public class Main {
         Document nbpPage = Jsoup.connect(nbpUrl).get();
         Elements rowsElements = nbpPage.select("#rightSide > table:nth-child(7) > tbody");
 
-//        ArrayList<Element> rowsList = new ArrayList<>();
 
         rowsElements.select("tr > td").eachText();
-//
         int count = 0;
             for (String e : rowsElements.select("tr > td").eachText()){
                 System.out.print(e);
@@ -30,38 +28,6 @@ public class Main {
                         break;
                 }
             }
-//        for (Element e : rowsElements) {
-//            System.out.println(e.select("tr > td").eachText());
-//        }
-//
-//        for (Element e : rowsElements){
-//            rowsList.add(e);
-//        }
-//
-//        for (Element e : rowsList){
-//            System.out.println(e.select("tr > td").eachText());
-//        }
-        
-        
-
-
-
-//        String urlUsd = "https://www.google.com/search?q=1+usd+to+pln&oq=1+usd+to+pln&aqs=chrome..69i57j0l7.2966j1j7&sourceid=chrome&ie=UTF-8";
-//        Document pageUsd = Jsoup.connect(urlUsd).get();
-//        Element link = pageUsd.select("#knowledge-currency__updatable-data-column > div.b1hJbf > div.dDoNo.vk_bk.gsrt.gzfeS > span.DFlfde.SwHCTb").first();
-//        System.out.println("1 Usd = "+link.text()+ " Pln");
-//
-//
-//        String urlEur = "https://www.google.com/search?q=1+eur+to+pln&oq=1+eur+to+pln&aqs=chrome..69i57j0l7.2129j1j9&sourceid=chrome&ie=UTF-8";
-//        Document pageEur = Jsoup.connect(urlEur).get();
-//        link = pageEur.select("#knowledge-currency__updatable-data-column > div.b1hJbf > div.dDoNo.vk_bk.gsrt.gzfeS > span.DFlfde.SwHCTb").first();
-//        System.out.println("1 Eur = "+link.text()+ " Pln");
-//
-//
-//        String urlTest = "https://biurwa.pl/";
-//        Document pageTest = Jsoup.connect(urlTest).get();
-//        link = pageEur.select("#sm-content > div.listing.clearfix > div:nth-child(1) > div.mpc_container.mpc_container_first > table > tbody > tr:nth-child(3) > td > div > h2 > a").first();
-//        System.out.println(link.text());
 
 
     }
